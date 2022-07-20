aefb546a1b8653819fb0dcb0c75fac32818e7485
Loongphy <Loongphy@outlook.com>
2022-07-20 23:10:29 +0800

    feat: update axios config

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-const serviceAxios = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 1000,
-  headers: { 'Content-Type': 'application/json' },
-})
-
-export default serviceAxios
+axios.defaults.timeout = 3000
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+export default axios
